3b0647b33c5c4a2a120e5823aa45dc6c69052e26
Mooneer Salem <mooneer@gmail.com>
2022-09-05 18:10:10 -0700

    Oops, used wrong content type for JS.

--- firmware/main/network/WirelessTask.cpp
+++ firmware/main/network/WirelessTask.cpp
@@ -143,7 +143,7 @@ static esp_err_t set_content_type_from_file(httpd_req_t *req, const char *filena
     }
     else if (IS_FILE_EXT(filename, ".js")) 
     {
-        return httpd_resp_set_type(req, "text/javascript");
+        return httpd_resp_set_type(req, "application/javascript");
     }
     /* This is a limited set only */
     /* For any other type always set as plain text */
