502aaa48c0dac74ba24d9ed1adab82475fc17b68
Mooneer Salem <mooneer@gmail.com>
2024-03-16 23:06:30 -0700

    Increase maximum number of open sockets in HTTP server to avoid intermittent loading errors while accessing the web interface.

--- firmware/main/network/HttpServerTask.cpp
+++ firmware/main/network/HttpServerTask.cpp
@@ -514,6 +514,7 @@ void HttpServerTask::onTaskStart_()
 
         // Allow HTTP server to auto-purge old connections.
         config.lru_purge_enable = true;
+        config.max_open_sockets = 10;
 
         /* Use the URI wildcard matching function in order to
         * allow the same handler to respond to multiple different
