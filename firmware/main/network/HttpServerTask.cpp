108b859481e87245fea346e84c50dcc7a58b726c
Mooneer Salem <mooneer@gmail.com>
2024-03-20 22:20:21 -0700

    Refactor file extension checking and add error handling.

--- firmware/main/network/HttpServerTask.cpp
+++ firmware/main/network/HttpServerTask.cpp
@@ -129,52 +129,63 @@ HttpServerTask::~HttpServerTask()
 
 #define IS_FILE_EXT(filename, ext) \
     (strcasecmp(&filename[strlen(filename) - sizeof(ext) + 1], ext) == 0)
-        
+
 /* Set HTTP response content type according to file extension */
-static esp_err_t set_content_type_from_file(httpd_req_t *req, const char *filename)
+static void set_content_type_from_file(httpd_req_t *req, const char *filename)
 {
-    // Enable unsafe inline scripting (required by newer Chrome)
-    httpd_resp_set_hdr(req, "Content-Security-Policy", "script-src 'self' 'unsafe-inline'");
+    bool gzipEncoding = false;
 
     if (IS_FILE_EXT(filename, ".pdf")) 
     {
-        return httpd_resp_set_type(req, "application/pdf");
+        ESP_ERROR_CHECK(httpd_resp_set_type(req, "application/pdf"));
     } 
     else if (IS_FILE_EXT(filename, ".html")) 
     {
-        return httpd_resp_set_type(req, "text/html");
+        ESP_ERROR_CHECK(httpd_resp_set_type(req, "text/html"));
     } 
     else if (IS_FILE_EXT(filename, ".jpeg")) 
     {
-        return httpd_resp_set_type(req, "image/jpeg");
+        ESP_ERROR_CHECK(httpd_resp_set_type(req, "image/jpeg"));
     } 
     else if (IS_FILE_EXT(filename, ".ico")) 
     {
-        return httpd_resp_set_type(req, "image/x-icon");
+        ESP_ERROR_CHECK(httpd_resp_set_type(req, "image/x-icon"));
     }
     else if (IS_FILE_EXT(filename, ".css")) 
     {
-        return httpd_resp_set_type(req, "text/css");
+        ESP_ERROR_CHECK(httpd_resp_set_type(req, "text/css"));
     }
     else if (IS_FILE_EXT(filename, ".js")) 
     {
-        return httpd_resp_set_type(req, "application/javascript");
+        ESP_ERROR_CHECK(httpd_resp_set_type(req, "application/javascript"));
     }
     else if (IS_FILE_EXT(filename, ".js.gz")) 
     {
         // Special case for compressed JavaScript
-        httpd_resp_set_hdr(req, "Content-Encoding", "gzip");
-        return httpd_resp_set_type(req, "application/javascript");
+        gzipEncoding = true;
+        ESP_ERROR_CHECK(httpd_resp_set_type(req, "application/javascript"));
     }
     else if (IS_FILE_EXT(filename, ".css.gz")) 
     {
         // Special case for compressed CSS
-        httpd_resp_set_hdr(req, "Content-Encoding", "gzip");
-        return httpd_resp_set_type(req, "text/css");
+        gzipEncoding = true;
+        ESP_ERROR_CHECK(httpd_resp_set_type(req, "text/css"));
     }
-    /* This is a limited set only */
-    /* For any other type always set as plain text */
-    return httpd_resp_set_type(req, "text/plain");
+    else
+    {
+        /* This is a limited set only */
+        /* For any other type always set as plain text */
+        ESP_ERROR_CHECK(httpd_resp_set_type(req, "text/plain"));
+    }
+
+    // Enable unsafe inline scripting (required by newer Chrome)
+    ESP_ERROR_CHECK(httpd_resp_set_hdr(req, "Content-Security-Policy", "script-src 'self' 'unsafe-inline'"));
+
+    // Enable gzip encoding if required
+    if (gzipEncoding)
+    {
+        ESP_ERROR_CHECK(httpd_resp_set_hdr(req, "Content-Encoding", "gzip"));
+    }
 }
 
 /* Copies the full path into destination buffer and returns
